bbc23f260fb54029975809870b208c6b2e8c0aea
Chandu JR <chandujr@gmail.com>
2025-02-12 01:19:37 +0530

    [UPDATE] change abort wait time for local LLMs

--- src/deep-research.ts
+++ src/deep-research.ts
@@ -104,7 +104,7 @@ async function processSerpResult({
 
   const res = await generateObject({
     model: o3MiniModel,
-    abortSignal: AbortSignal.timeout(60_000),
+    abortSignal: AbortSignal.timeout(6_000_000),
     system: systemPrompt(),
     prompt: `Given the following contents from a SERP search for the query <query>${query}</query>, generate a list of learnings from the contents. Return a maximum of ${numLearnings} learnings, but feel free to return less if the contents are clear. Make sure each learning is unique and not similar to each other. The learnings should be concise and to the point, as detailed and information dense as possible. Make sure to include any entities like people, places, companies, products, things, etc in the learnings, as well as any exact metrics, numbers, or dates. The learnings will be used to research the topic further.\n\n<contents>${contents
       .map(content => `<content>\n${content}\n</content>`)
